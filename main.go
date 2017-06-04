05de04603b668bb85880e73fba540878377fb240
Felix Breidenstein <mail@felixbreidenstein.de>
2017-06-04 23:53:53 +0200

    Use codebase for an GitLab IRC Bot. Currently just handles push events

--- main.go
+++ main.go
@@ -19,7 +19,6 @@ import (
 var (
 	host     = flag.String("host", "irc.hackint.org", "Hostname of the IRC server")
 	port     = flag.Int("sslport", 6697, "SSL capable port of the IRC server")
-	channel  = flag.String("channel", "", "Target to send notifications to, likely a channel.")
 	nickname = flag.String("nickname", "go-prom-irc", "Nickname to assume once connected")
 	gecos    = flag.String("gecos", "go-prom-irc", "Realname to assume once connected")
 	cafile   = flag.String("cafile", "", "Path to the ca file that verifies the server certificate.")
@@ -27,67 +26,85 @@ var (
 
 func CreateFunctionNotifyFunction(bot *irc.Connection) http.HandlerFunc {
 
-	const templateString = "{{ .Alert.Labels.instance }} {{ .ColorStart }}{{ .Alert.Labels.alertname}}{{ .ColorEnd }} - {{ .Alert.Annotations.description}}"
+	const pushString = "[\x0311 {{ .Project.Name }} \x03] {{ .UserName }} pushed {{ .TotalCommits }} new commits to \x0305{{ .Project.Branch }}\x03"
+	const commitString = "\x0315{{ .ShortID }}\x03 (\x0303{{ .AddedFiles }}\x03,\x0308{{ .ModifiedFiles }}\x03,\x0304{{ .RemovedFiles }}\x03) - {{ .Message }}"
 
-	notificationTemplate, err := template.New("notification").Parse(templateString)
+	pushTemplate, err := template.New("push notification").Parse(pushString)
 	if err != nil {
-		log.Fatalf("Failed to parse template: %v", err)
+		log.Fatalf("Failed to parse pushEvent template: %v", err)
+	}
+
+	commitTemplate, err := template.New("commit notification").Parse(commitString)
+	if err != nil {
+		log.Fatalf("Failed to parse commitString template: %v", err)
 	}
 
 	return func(wr http.ResponseWriter, req *http.Request) {
 		defer req.Body.Close()
 		decoder := json.NewDecoder(req.Body)
 
-		type Alert struct {
-			Labels      map[string]interface{} `json:"labels"`
-			Annotations map[string]interface{} `json:"annotations"`
-			StartsAt    string                 `json:"startsAt"`
-			EndsAt      string                 `json:"endsAt"`
+		var eventType = req.Header["X-Gitlab-Event"][0]
+
+		type Project struct {
+			Name   string `json:"name"`
+			Branch string `json:"default_branch"`
 		}
 
-		type Notification struct {
-			Version           string                 `json:"version"`
-			GroupKey          uint64                 `json:"groupKey"`
-			Status            string                 `json:"status"`
-			Receiver          string                 `json:"receiver"`
-			GroupLables       map[string]interface{} `json:"groupLabels"`
-			CommonLabels      map[string]interface{} `json:"commonLabels"`
-			CommonAnnotations map[string]interface{} `json:"commonAnnotations"`
-			ExternalURL       string                 `json:"externalURL"`
-			Alerts            []Alert                `json:"alerts"`
+		type Commit struct {
+			Id       string   `json:"id"`
+			Message  string   `json:"message"`
+			Added    []string `"json:"added"`
+			Modified []string `"json:"modified"`
+			Removed  []string `"json:"removed"`
 		}
 
-		var notification Notification
-
-		if err := decoder.Decode(&notification); err != nil {
-			log.Println(err)
-			return
+		type PushEvent struct {
+			UserName     string   `json:"user_name"`
+			Project      Project  `json:"project"`
+			Commits      []Commit `json:"commits"`
+			TotalCommits int      `json:"total_commits_count"`
 		}
 
-		body, err := json.Marshal(&notification)
+		switch eventType {
 
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		log.Printf("JSON: %v", string(body))
-		for _, alert := range notification.Alerts {
-			type NotificationContext struct {
-				Alert        *Alert
-				Notification *Notification
-				ColorStart   string
-				ColorEnd     string
+		case "Push Hook":
+			var pushEvent PushEvent
+			if err := decoder.Decode(&pushEvent); err != nil {
+				log.Println(err)
+				return
 			}
-			context := NotificationContext{
-				Alert:        &alert,
-				Notification: &notification,
-				ColorStart:   notification.Status,
-				ColorEnd:     "\x03",
-			}
-
 			var buf bytes.Buffer
-			err = notificationTemplate.Execute(&buf, &context)
-			bot.Privmsg(*channel, buf.String())
+			err = pushTemplate.Execute(&buf, &pushEvent)
+			bot.Privmsg("#fleaz", buf.String())
+
+			for _, commit := range pushEvent.Commits {
+				type CommitContext struct {
+					ShortID       string
+					Message       string
+					AddedFiles    int
+					ModifiedFiles int
+					RemovedFiles  int
+				}
+
+				context := CommitContext{
+					ShortID:       commit.Id[len(commit.Id)-8:],
+					Message:       commit.Message,
+					AddedFiles:    len(commit.Added),
+					ModifiedFiles: len(commit.Modified),
+					RemovedFiles:  len(commit.Removed),
+				}
+
+				var buf bytes.Buffer
+				err = commitTemplate.Execute(&buf, &context)
+				if err != nil {
+					log.Printf("ERROR: %v", err)
+				}
+				bot.Privmsg("#fleaz", buf.String())
+
+			}
+
+		default:
+			log.Printf("Unknown event: %s", eventType)
 		}
 
 	}
@@ -136,7 +153,7 @@ func main() {
 
 func RegisterHandlers(irccon *irc.Connection) {
 	irccon.AddCallback("001", func(e *irc.Event) {
-		log.Printf("Joining %v", channel)
+		log.Printf("Joining %v", *channel)
 		irccon.Join(*channel)
 	})
 	irccon.AddCallback("366", func(e *irc.Event) {})
